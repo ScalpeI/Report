5a836328f5fe024eca38552a0d5302c9d5519053
ScalpeI <60978659+ScalpeI@users.noreply.github.com>
2020-02-13 10:17:10 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Report_on_issued_policies
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.dtpEnd = new System.Windows.Forms.DateTimePicker();
+            this.dtpNewborn = new System.Windows.Forms.DateTimePicker();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.lblEnd = new System.Windows.Forms.Label();
+            this.lblNewborn = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.CustomFormat = "";
+            this.dtpStart.Location = new System.Drawing.Point(132, 22);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(142, 20);
+            this.dtpStart.TabIndex = 0;
+            // 
+            // dtpEnd
+            // 
+            this.dtpEnd.Location = new System.Drawing.Point(132, 47);
+            this.dtpEnd.Name = "dtpEnd";
+            this.dtpEnd.Size = new System.Drawing.Size(142, 20);
+            this.dtpEnd.TabIndex = 1;
+            // 
+            // dtpNewborn
+            // 
+            this.dtpNewborn.Location = new System.Drawing.Point(132, 73);
+            this.dtpNewborn.Name = "dtpNewborn";
+            this.dtpNewborn.Size = new System.Drawing.Size(142, 20);
+            this.dtpNewborn.TabIndex = 2;
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Location = new System.Drawing.Point(25, 25);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(89, 13);
+            this.lblStart.TabIndex = 3;
+            this.lblStart.Text = "Начало периода";
+            // 
+            // lblEnd
+            // 
+            this.lblEnd.AutoSize = true;
+            this.lblEnd.Location = new System.Drawing.Point(25, 50);
+            this.lblEnd.Name = "lblEnd";
+            this.lblEnd.Size = new System.Drawing.Size(83, 13);
+            this.lblEnd.TabIndex = 4;
+            this.lblEnd.Text = "Конец периода";
+            // 
+            // lblNewborn
+            // 
+            this.lblNewborn.AutoSize = true;
+            this.lblNewborn.Location = new System.Drawing.Point(25, 76);
+            this.lblNewborn.Name = "lblNewborn";
+            this.lblNewborn.Size = new System.Drawing.Size(91, 13);
+            this.lblNewborn.TabIndex = 5;
+            this.lblNewborn.Text = "Новорожденные";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(44, 108);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(212, 37);
+            this.btnCreate.TabIndex = 6;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 161);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.lblNewborn);
+            this.Controls.Add(this.lblEnd);
+            this.Controls.Add(this.lblStart);
+            this.Controls.Add(this.dtpNewborn);
+            this.Controls.Add(this.dtpEnd);
+            this.Controls.Add(this.dtpStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Report on issued policies";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.DateTimePicker dtpEnd;
+        private System.Windows.Forms.DateTimePicker dtpNewborn;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.Label lblEnd;
+        private System.Windows.Forms.Label lblNewborn;
+        private System.Windows.Forms.Button btnCreate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Report_on_issued_policies
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        //Екземпляр приложения Excel
+        Excel.Application xlApp;
+        //Лист
+        Excel.Worksheet xlSheet;
+        //Выделеная область
+        Excel.Range xlSheetRange;
+
+        private DataTable GetData()
+        {
+            //строка соединения
+            string connString = @"Network Library=DBMSSOCN;Data Source=192.168.1.101;Initial Catalog=Oms_Buryatiya;User ID=sa;Password=Kmsadmin_403;Persist Security Info=True;";
+
+            SqlConnection con = new SqlConnection(connString);
+            DataTable dt = new DataTable();
+            try
+            {
+                string query =
+                  @"declare @DR as datetime
+                    declare @DB as datetime
+                    declare @DE as datetime
+
+                    set @DB = '" + dtpStart.Value + @"'
+                    set @DR = '" + dtpNewborn.Value + @"'
+                    set @DE = '" + dtpEnd.Value + @"'
+
+                    Select
+                        r.Name as Офис,
+	                    p.IDPRZ as ПВП,
+	                    sum(case when p.PolisType = '2' and s.StatementType = '1' and s.StatementWork = '1' then 1 else 0 end) as Выбор,
+	                    sum(case when p.PolisType = '2' and s.StatementType = '2' and s.StatementWork = '1' then 1 else 0 end) as Переоформление,
+	                    sum(case when p.PolisType = '2' and s.StatementType = '2' and s.StatementWork = '2' then 1 else 0 end) as Дубликат,
+	                    sum(case when p.PolisType = '2' and (s.StatementType = '1') and (s.StatementWork = '4' or s.StatementWork = '2' or s.StatementWork = '3') and s.StatementBase != '0' then 1 else 0 end) as  С_переизготовлением,
+	                    sum(case when p.PolisType = '3' and (s.StatementType = '1') and (s.StatementWork = '4' or s.StatementWork = '2' or s.StatementWork = '3') and s.StatementBase = '0' then 1 else 0 end) as Без_переизготовлением,
+	                    sum(case when (s.Birthday>= @DR and s.Birthday< @DE) and p.PolisType = '2' and (s.StatementWork = '1' or s.StatementWork = '4' or s.StatementWork = '2' or s.StatementWork = '3') then 1 else 0 end) as Новорожденные,
+	                    sum(case when (p.PolisType = '2' and s.StatementType = '1' and s.StatementWork = '1') or 
+	                        (p.PolisType = '2' and (s.StatementType = '1') and (s.StatementWork = '4' or s.StatementWork = '2' or s.StatementWork = '3') and s.StatementBase != '0') or 
+	                        (p.PolisType = '3' and (s.StatementType = '1') and (s.StatementWork = '4' or s.StatementWork = '2' or s.StatementWork = '3') and s.StatementBase = '0')	then 1 else 0 end) as ВСЕГО
+	                from Polis as p
+	                    left join
+	                        Statement as s
+	                    on
+	                        p.IDPolis = s.IdPolis 
+	                    left join
+	                        PRZ as r
+	                    on
+	                        p.IDPRZ = r.IDPRZ
+	                    left join
+	                        PravaPLZ as w
+	                    on
+	                        p.IDOperator = w.PrUserID
+	                where 
+		                p.PolisDate >= @DB
+			            and
+			            p.PolisDate <= @DE
+
+	                group by p.IDPRZ,r.Name
+	                order by p.IDPRZ";
+
+                SqlCommand comm = new SqlCommand(query, con);
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter(comm);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dt = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+            return dt;
+        }
+
+        private DataTable GetSum()
+        {
+            //строка соединения
+            string connString = @"Network Library=DBMSSOCN;Data Source=192.168.1.101;Initial Catalog=Oms_Buryatiya;User ID=sa;Password=Kmsadmin_403;Persist Security Info=True;";
+
+            SqlConnection con = new SqlConnection(connString);
+            DataTable dtsum = new DataTable();
+            try
+            {
+                string query =
+                  @"declare @DR as datetime
+                    declare @DB as datetime
+                    declare @DE as datetime
+
+                    set @DB = '" + dtpStart.Value + @"'
+                    set @DR = '" + dtpNewborn.Value + @"'
+                    set @DE = '" + dtpEnd.Value + @"'
+
+                    Select
+                        'ИТОГО',
+	                    '',
+	                    sum(case when p.PolisType = '2' and s.StatementType = '1' and s.StatementWork = '1' then 1 else 0 end) as Выбор,
+	                    sum(case when p.PolisType = '2' and s.StatementType = '2' and s.StatementWork = '1' then 1 else 0 end) as Переоформление,
+	                    sum(case when p.PolisType = '2' and s.StatementType = '2' and s.StatementWork = '2' then 1 else 0 end) as Дубликат,
+	                    sum(case when p.PolisType = '2' and (s.StatementType = '1') and (s.StatementWork = '4' or s.StatementWork = '2' or s.StatementWork = '3') and s.StatementBase != '0' then 1 else 0 end) as  С_переизготовлением,
+	                    sum(case when p.PolisType = '3' and (s.StatementType = '1') and (s.StatementWork = '4' or s.StatementWork = '2' or s.StatementWork = '3') and s.StatementBase = '0' then 1 else 0 end) as Без_переизготовлением,
+	                    sum(case when (s.Birthday>= @DR and s.Birthday< @DE) and p.PolisType = '2' and (s.StatementWork = '1' or s.StatementWork = '4' or s.StatementWork = '2' or s.StatementWork = '3') then 1 else 0 end) as Новорожденные,
+	                    sum(case when (p.PolisType = '2' and s.StatementType = '1' and s.StatementWork = '1') or 
+	                        (p.PolisType = '2' and (s.StatementType = '1') and (s.StatementWork = '4' or s.StatementWork = '2' or s.StatementWork = '3') and s.StatementBase != '0') or 
+	                        (p.PolisType = '3' and (s.StatementType = '1') and (s.StatementWork = '4' or s.StatementWork = '2' or s.StatementWork = '3') and s.StatementBase = '0')	then 1 else 0 end) as ВСЕГО
+	                from Polis as p
+	                    left join
+	                        Statement as s
+	                    on
+	                        p.IDPolis = s.IdPolis 
+	                    left join
+	                        PRZ as r
+	                    on
+	                        p.IDPRZ = r.IDPRZ
+	                    left join
+	                        PravaPLZ as w
+	                    on
+	                        p.IDOperator = w.PrUserID
+	                where 
+		                p.PolisDate >= @DB
+			            and
+			            p.PolisDate <= @DE";
+
+                SqlCommand comm = new SqlCommand(query, con);
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter(comm);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dtsum = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+            return dtsum;
+        }
+
+        void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show(ex.ToString(), "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            xlApp = new Excel.Application();
+
+            try
+            {
+                //добавляем книгу
+                xlApp.Workbooks.Add(Type.Missing);
+
+                //делаем временно неактивным документ
+                xlApp.Interactive = false;
+                xlApp.EnableEvents = false;
+
+                //выбираем лист на котором будем работать (Лист 1)
+                xlSheet = (Excel.Worksheet)xlApp.Sheets[1];
+                //Название листа
+                xlSheet.Name = "Данные";
+
+                //Выгрузка данных
+                DataTable dt = GetData();
+                DataTable dtsum = GetSum();
+
+                int collInd = 0;
+                int rowInd = 0;
+                string data = "";
+
+                //называем колонки
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    data = dt.Columns[i].ColumnName.ToString();
+                    xlSheet.Cells[1, i + 1] = data;
+
+                    //выделяем первую строку
+                    xlSheetRange = xlSheet.get_Range("A1:Z1", Type.Missing);
+
+                    //делаем полужирный текст и перенос слов
+                    xlSheetRange.WrapText = true;
+                    xlSheetRange.Font.Bold = true;
+                }
+
+                //заполняем строки
+                for (rowInd = 0; rowInd < dt.Rows.Count; rowInd++)
+                {
+                    for (collInd = 0; collInd < dt.Columns.Count; collInd++)
+                    {
+                        data = dt.Rows[rowInd].ItemArray[collInd].ToString();
+                        xlSheet.Cells[rowInd + 2, collInd + 1] = data;
+                    }
+                }
+                for (rowInd = 0; rowInd < dtsum.Rows.Count; rowInd++)
+                {
+                    for (collInd = 0; collInd < dtsum.Columns.Count; collInd++)
+                    {
+                        data = dtsum.Rows[rowInd].ItemArray[collInd].ToString();
+                        xlSheet.Cells[rowInd + dt.Rows.Count + 2, collInd +  1] = data;
+                    }
+                }
+
+                //выбираем всю область данных
+                xlSheetRange = xlSheet.UsedRange;
+
+                //выравниваем строки и колонки по их содержимому
+                xlSheetRange.Columns.AutoFit();
+                xlSheetRange.Rows.AutoFit();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                //Показываем ексель
+                xlApp.Visible = true;
+
+                xlApp.Interactive = true;
+                xlApp.ScreenUpdating = true;
+                xlApp.UserControl = true;
+
+                //Отсоединяемся от Excel
+                releaseObject(xlSheetRange);
+                releaseObject(xlSheet);
+                releaseObject(xlApp);
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            var year = DateTime.Now.Year;
+            var month = DateTime.Now.Month;
+            dtpStart.Value = new DateTime(year, month, 1);
+            dtpEnd.Value = new DateTime(year, month, 31);
+            dtpNewborn.Value = new DateTime(year, 1, 1);
+        }
+    }
+}
